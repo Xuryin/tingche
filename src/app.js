869029fa04fbb22219cfc93328094d74042a4ef9
ruzeqing <ruzeqing@yunyang.com.cn>
2020-07-27 14:06:40 +0800

    添加token信息

--- src/app.js
+++ src/app.js
@@ -18,18 +18,18 @@ import './app.styl'
 export default class App extends Component {
   render() {
     return (
-    // <Auth>
-      <Router>
-        <Framework>
-          <Switch>
-            <Route excat path={'/hello'} component={Hello} />
-            <Redirect exact from={'/'} to={'/hello'} />
-            <div>nihaoa </div>
-            <Route render={() => <div className="FBV FBAC FBJC" style={{fontSize: 100}}>404</div>} />
-          </Switch>
-        </Framework>
-      </Router>
-    // </Auth>
+      <Auth>
+        <Router>
+          <Framework>
+            <Switch>
+              <Route excat path={'/hello'} component={Hello} />
+              <Redirect exact from={'/'} to={'/hello'} />
+              <div>nihaoa </div>
+              <Route render={() => <div className="FBV FBAC FBJC" style={{fontSize: 100}}>404</div>} />
+            </Switch>
+          </Framework>
+        </Router>
+      </Auth>
     )
   }
 }
